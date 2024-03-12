a56439a74c972e90ec0f9872a4affb9600982600
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-12 22:22:19 +0500

    Add files via upload

---
+++ Ifelse.ts
@@ -0,0 +1,67 @@
+               //  logic statement  \\
+          //  statement of if else (class assignment)
+
+let smartphone : string = 'Samsung A32';
+if (smartphone) {
+         console.log ("MODEL NO Samsung Galaxy A32 4G SM-A325F/DS 16.3 cm (6.4) Dual SIM Android 11 USB Type-C 4 GB 128 GB 5000 mAh Black")
+} else {
+           console.log ('No model number founded')
+};
+
+
+let a_car : string = 'civic 2020'
+if (a_car){
+   
+    console.log ('2020 honda civic Sedan')
+} else  {
+
+    console.log ('honda civic oriel 1.8-VTEC CVT')
+};
+
+            //   statement if else in number 
+
+let number : number = 120
+if (number == 120) {
+      console.log ('congratulation you got a sugar candy.')
+
+}     else {
+
+    console.log ('the sugar candy price is 120rs ')
+};      
+
+
+    
+
+//   Else if statement
+
+let months : string = 'RAMADAN'
+if 
+(months === months){
+
+
+    console.log ('RAMADAN MUBARAK')
+
+} else if 
+
+    (months === 'Shawwal'){
+    
+        console.log ('Eid-ul-Fitr Mubarak')
+} else {
+   
+    console.log ('Ramadan is currently ongoing')
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
