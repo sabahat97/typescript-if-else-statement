8f37ed40a07d3f1f4e1f51547c130226352f00a5
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-12 22:49:24 +0500

    Add files via upload

---
+++ ifelsee.ts
@@ -0,0 +1,12 @@
+
+let rollno = 13479
+if (rollno > 13478){
+  
+    console.log ('seat of A-B alphabeat students')
+}else if (rollno < 13475){
+    
+    console.log ('seat of C-D alphabeat students') //no output 
+} else (rollno <13475)
+{
+    console.log ('The seat of unknown sudents')
+};
