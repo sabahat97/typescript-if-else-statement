df373eb48f1d0e0ef650a7ab15d3cf06db5350ea
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-14 22:59:45 +0500

    Update ifelsee.ts

--- ifelsee.ts
+++ ifelsee.ts
@@ -1,12 +1,12 @@
 
 let rollno = 13479
-if (rollno > 13478){
+if (rollno > 13479){
   
-    console.log ('seat of A-B alphabeat students')
-}else if (rollno < 13475){
+    console.log ('seat of A-B alphabet students')
+}else if (rollno < 13480){
     
-    console.log ('seat of C-D alphabeat students') //no output 
-} else (rollno <13475)
+    console.log ('seat of C-D alphabet students') //no output 
+} else (rollno <13490)
 {
     console.log ('The seat of unknown sudents')
 };
